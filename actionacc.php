10b111226ce9cd8f1b8386321e7e3ab9ed351aea
muhamaz <humamzaky7@gmail.com>
2021-09-17 14:44:36 +0700

    Memperbaiki Celah

--- actionacc.php
+++ actionacc.php
@@ -3,7 +3,15 @@
     require 'koneksi.php';
 
     $qAcc = "UPDATE izin SET isAccepted = 2 WHERE id_izin = ".$_GET['id_izin'];
-    $conn->query($qAcc);
-    $conn->close();
-    header("Location:admin.php");
+
+    if($_SESSION['isAdmin'] != 2) {
+        header("Location:dashboard.php");
+        $conn->close();
+        exit;
+    } else {
+        $conn->query($qAcc);
+        $conn->close();
+        header("Location:admin.php");
+    }
+
 ?>
